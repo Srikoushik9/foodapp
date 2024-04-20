c410d999f06b6c2bd68ed8621b7b6395863649a6
Srikoushik9 <87562795+Srikoushik9@users.noreply.github.com>
2024-04-20 00:48:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ app.register_blueprint(auth_bp)
 
 
 if __name__ == '__main__':
-    #app.run(debug=True,host='0.0.0.0',port=10000)
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(debug=True,host='0.0.0.0',port=10000)
+    # http_server = WSGIServer(('', 5000), app)
+    # http_server.serve_forever()
