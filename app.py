c03d925dcd5e448d71f6bf0c9e3835293254c950
Srikoushik9 <87562795+Srikoushik9@users.noreply.github.com>
2024-04-20 00:37:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request
+from gevent.pywsgi import WSGIServer
 
 from view import auth_bp
 
@@ -10,4 +11,6 @@ app.register_blueprint(auth_bp)
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=10000)
+    #app.run(debug=True,host='0.0.0.0',port=10000)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
